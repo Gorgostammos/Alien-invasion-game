94fa56caee37c3cab83ff2acc4a68990dece65ac
Gorgos Tammo <gorgos2002@icloud.com>
2025-05-02 16:16:42 +0200

    jeg har aliens nå

--- setting.py
+++ setting.py
@@ -8,12 +8,18 @@ class Settings:
         self.bg_color = (28, 37, 60)  # Bakgrunnsfargen (RGB-verdi)
 
         # Skipets innstillinger
-        self.ship_speed = 0.2  # Hvor raskt skipet beveger seg
+        self.ship_speed = 1.5  # Hvor raskt skipet beveger seg
+
+        self.alien_speed = 1.0
+
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         # Bullet settings
 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 3
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
         self.bullet_color = (247, 246, 187)
